f854abda5ca965d2d8f7f2f51bb968e45fb0be97
CSGA <67216376+CSGA-iOS@users.noreply.github.com>
2022-08-17 14:31:20 +0900

    Delete assets directory

--- assets/js/tawk.js
+++
@@ -1,9 +0,0 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/625edc3e7b967b11798b765e/1g118olun';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
